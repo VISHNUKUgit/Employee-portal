705755ebae09d5790ca8700b2376aa29c3798a23
unknown <VISHNUKUgit>
2023-12-07 22:02:05 +0530

    07-12

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+email:String=""
+password:String =""
+login(){
+  if (this.email && this.password) {
+    alert("Proceed")
+  } else {
+    alert("fill all")
+  }
+}
 }
